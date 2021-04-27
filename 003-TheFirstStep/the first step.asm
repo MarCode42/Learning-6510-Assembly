6844e0218453be6874716836184a21f203424858
Marcus_Coder <marcode42x@protonmail.com>
2021-04-27 20:51:51 +0200

    added comments to the code

--- 003-TheFirstStep/the first step.asm
+++ 003-TheFirstStep/the first step.asm
@@ -1,11 +1,17 @@
- *=$0801
- !byte $0c,$08,$e2,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
- 
- lda #$41
- ldx #$ff
- 
+;*** variables
+SCREEN  = $0400         ;start of screen memory
+CHAR    = $41           ;spade character for output
+
+;*** start address, BASIC line
+*=$0801
+        !byte $0c,$08,$e2,$07,$9e,$20,$32,$30,$36,$32,$00,$00,$00
+
+;*** start of assembly program
+        lda #CHAR       ;load character into A
+        ldx #$ff        ;loop counter
+
 loop
- sta $03ff,x
- dex
- bne loop
- rts
+        sta SCREEN-1,x  ;output A to screen
+        dex           
+        bne loop
+        rts
